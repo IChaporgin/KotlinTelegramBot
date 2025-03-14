906010c6f664c587634e22e7a2627f3c1a6d8002
Igor Chaporgin <fellclub@yandex.ru>
2025-03-14 21:59:25 +0300

    KTB-16 creating menu and send choose

--- src/main/kotlin/TelegramBotService.kt
+++ src/main/kotlin/TelegramBotService.kt
@@ -1,7 +1,9 @@
 import java.net.URI
+import java.net.URLEncoder
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
+import java.nio.charset.StandardCharsets
 
 class TelegramBotService(
     private val botToken: String,
@@ -17,11 +19,57 @@ class TelegramBotService(
     }
 
     fun sendMessage(chatID: String, message: String): String {
-        val url = "$TELEGRAM_URL$botToken/sendMessage?chat_id=$chatID&text=$message"
+        val encodedMessage = URLEncoder.encode(message, StandardCharsets.UTF_8.toString()) // Кодируем текст
+        val url = "$TELEGRAM_URL$botToken/sendMessage?chat_id=$chatID&text=$encodedMessage"
+//        val url = "$TELEGRAM_URL$botToken/sendMessage?chat_id=$chatID&text=$message"
         val request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(url)).build()
         val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+        val responseBody = response.body()
+        println("Response: $responseBody")
 
         return response.body()
     }
+
+    fun sendMenu(chatID: String): String {
+        val url = "$TELEGRAM_URL$botToken/sendMessage"
+
+        // Корректный JSON для Inline-клавиатуры
+        val sendMenuBody = """
+        {
+          "chat_id": "$chatID",
+          "text": "Основное меню",
+          "reply_markup": {
+            "inline_keyboard": [
+              [
+                {
+                  "text": "Изучить слова",
+                  "callback_data": "learning_words"
+                }
+              ],
+              [
+                {
+                  "text": "Статистика",
+                  "callback_data": "statistics"
+                }
+              ]
+            ]
+          }
+        }
+    """.trimIndent()
+
+        val request: HttpRequest = HttpRequest.newBuilder()
+            .uri(URI.create(url))
+            .header("Content-Type", "application/json; charset=UTF-8")
+            .POST(HttpRequest.BodyPublishers.ofString(sendMenuBody, StandardCharsets.UTF_8))
+            .build()
+
+        val response = client.send(request, HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8))
+        val responseBody = response.body()
+        println("Response: $responseBody")
+
+        return response.body()
+
+    }
+
 }
 private const val TELEGRAM_URL = "https://api.telegram.org/bot"
